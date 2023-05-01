1bae9c6e1859b775c6d5fe3253859973ba7c0b7b
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2023-05-01 12:04:07 +0300

    update

--- src/components/cases/slider/Slider.jsx
+++ src/components/cases/slider/Slider.jsx
@@ -1,5 +1,13 @@
 import { Navigation, EffectFade } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  GalleryItem1,
+  GalleryItem2,
+  GalleryItem3,
+  GalleryItem4,
+  GalleryItem5,
+  GalleryItem6,
+} from '../GalleryItems';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -18,100 +26,22 @@ function Slider() {
       loop
     >
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases1.webp 1x, ../src/assets/cases/cases1@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases1.jpg'
-            srcSet='../src/assets/cases/cases1@2x.jpg 2x'
-            alt='People in a meeting'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem1 />
       </SwiperSlide>
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases2.webp 1x, ../src/assets/cases/cases2@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases2.jpg'
-            srcSet='../src/assets/cases/cases1@2x.jpg 2x'
-            alt='Woman with laptop'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem2 />
       </SwiperSlide>
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases3.webp 1x, ../src/assets/cases/cases3@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases3.jpg'
-            srcSet='../src/assets/cases/cases3@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem3 />
       </SwiperSlide>
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases4.webp 1x, ../src/assets/cases/cases4@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases4.jpg'
-            srcSet='../src/assets/cases/cases4@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem4 />
       </SwiperSlide>
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases5.webp 1x, ../src/assets/cases/cases5@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases5.jpg'
-            srcSet='../src/assets/cases/cases5@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem5 />
       </SwiperSlide>
       <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases6.webp 1x, ../src/assets/cases/cases6@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases6.jpg'
-            srcSet='../src/assets/cases/cases6@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
-      </SwiperSlide>
-      <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases7.webp 1x, ../src/assets/cases/cases7@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases7.jpg'
-            srcSet='../src/assets/cases/cases7@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
-      </SwiperSlide>
-      <SwiperSlide>
-        <picture>
-          <source srcSet='../src/assets/cases/cases8.webp 1x, ../src/assets/cases/cases8@2x.webp 2x' type='image/webp' />
-          <img
-            className='cases-img'
-            src='../src/assets/cases/cases8.jpg'
-            srcSet='../src/assets/cases/cases8@2x.jpg 2x'
-            alt='Writing in notepad'
-            loading='lazy'
-          />
-        </picture>
+        <GalleryItem6 />
       </SwiperSlide>
     </Swiper>
   );
